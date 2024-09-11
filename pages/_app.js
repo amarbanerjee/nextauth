276b043cd18cb8e10ae0dc7aad1e2ea4d379d562
unknown <amar.banerjee@gmail.com>
2024-09-11 12:59:08 +0530

    First Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
-import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { SessionProvider } from 'next-auth/react';
+import Layout from '../components/layout/layout';
+import '../styles/globals.css';
+import { NotificationContextProvider } from '../store/notification-context';
+
+function MyApp({ Component, pageProps }) {
+  return (
+
+    <NotificationContextProvider>
+    <SessionProvider session={pageProps.session}>
+      
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      
+    </SessionProvider>
+    </NotificationContextProvider>
+    
+  );
 }
+
+export default MyApp;
